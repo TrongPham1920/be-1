31d5a1b92b150556fc0f656d7e05883ad0b6d02c
TRONG\GIGABYTE <phamtrong1920@gmail.com>
2024-07-03 22:37:36 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ const express = require("express");
 const MongoDB = require("./database/mongodb");
 const router = require("./server/router");
 
+const cors = require("cors");
+
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions = { origin: true };
+  callback(null, corsOptions);
+};
+
 const app = express();
 
 app.use(express.json());
-
+app.use(cors(corsOptionsDelegate));
 app.use(router);
 
 module.exports = app;
